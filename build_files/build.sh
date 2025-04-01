8169376e7a19c0004663c99a35904dabbb942441
thesaigoneer <165976396+thesaigoneer@users.noreply.github.com>
2025-04-01 14:57:57 +0700

    Update build.sh - final tweaks to packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,21 +9,26 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y hyprland
-
-# Use a COPR Example:
-#
+# Enable the necessary COPR's:
 dnf5 -y copr enable tofik/sway
 dnf5 -y copr enable tofik/golang
 dnf5 -y copr enable erikreider/SwayNotificationCenter
 dnf5 -y copr enable mochaa/gtk-session-lock
 dnf5 -y copr enable tofik/nwg-shell
-dnf5 -y install nwg-shell
-dnf5 -y install azote
-dnf5 -y install rofi-wayland
-dnf5 -y install kitty
-dnf5 -y install afetch
+
+# Add and remove packages to the image:
+dnf5 install -y azote
+dnf5 install -y blueman
+dnf5 install -y fastfetch
+dnf5 install -y hyprland
+dnf5 install -y kitty
+dnf5 install -y mc
+dnf5 install -y micro
+dnf5 install -y nwg-shell
+
+dnf5 remove -y firefox
+dnf5 remove -y foot
+dnf5 remove -y nvtop
 
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
