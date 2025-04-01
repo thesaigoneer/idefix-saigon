83607bd0471e0ee1a365e922078fb90403d6ea8d
thesaigoneer <165976396+thesaigoneer@users.noreply.github.com>
2025-04-01 08:23:37 +0700

    Update build.sh - adding copr's for nwg-shell

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,10 +14,25 @@ dnf5 install -y hyprland
 
 # Use a COPR Example:
 #
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+dnf5 -y copr enable https://copr.fedorainfracloud.org/coprs/tofik/sway/
+dnf5 -y copr enable https://copr.fedorainfracloud.org/coprs/tofik/golang/
+dnf5 -y copr enable https://copr.fedorainfracloud.org/coprs/erikreider/SwayNotificationCenter/
+dnf5 -y copr enable https://copr.fedorainfracloud.org/coprs/mochaa/gtk-session-lock/
+dnf5 -y copr enable https://copr.fedorainfracloud.org/coprs/tofik/nwg-shell/
+dnf5 -y install nwg-shell
+dnf5 -y install azote
+dnf5 -y install rofi-wayland
+dnf5 -y install kitty
+dnf5 -y install afetch
+
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable https://copr.fedorainfracloud.org/coprs/tofik/sway/
+dnf5 -y copr disable https://copr.fedorainfracloud.org/coprs/tofik/golang/
+dnf5 -y copr disable https://copr.fedorainfracloud.org/coprs/erikreider/SwayNotificationCenter/
+dnf5 -y copr disable https://copr.fedorainfracloud.org/coprs/mochaa/gtk-session-lock/
+dnf5 -y copr disable https://copr.fedorainfracloud.org/coprs/tofik/nwg-shell/
+
 
 #### Example for enabling a System Unit File
 
